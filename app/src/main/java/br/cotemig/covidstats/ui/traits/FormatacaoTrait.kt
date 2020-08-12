a27de5b828021448c1ae93767241f3966c2c32e2
Pedro José <pedrojosepaiva@outlook.com>
2020-08-12 17:15:48 -0300

    Update FormatacaoTrait.kt

--- app/src/main/java/br/cotemig/covidstats/ui/traits/FormatacaoTrait.kt
+++ app/src/main/java/br/cotemig/covidstats/ui/traits/FormatacaoTrait.kt
@@ -3,10 +3,10 @@ package br.cotemig.covidstats.ui.traits
 import java.text.NumberFormat
 import java.util.*
 
-public class FormatacaoTrait (){
+class FormatacaoTrait (){
+    var numberFormat = NumberFormat.getNumberInstance(Locale("pt","BR"))
 
     fun formatarNumero(numero: Int): String{
-        var numberFormat = NumberFormat.getNumberInstance(Locale("pt","BR"))
         var numeroFormatado = numberFormat.format(numero)
         return numeroFormatado.toString()
     }
